42f866acc7b0bd689ca5d32d1e10e72b43833081
Fabrice Lawson-Zankli <fabricelawson-zankli@MacBook-Air-von-Fabrice.local>
2025-10-15 08:20:43 +0200

    Erste Übung

---
+++ Greeting.java
@@ -0,0 +1,6 @@
+public class Greeting {
+  public static void main(String[] args) {
+    System.out.println("Fabrice Lawson-Zankli:");
+    System.out.print("Informatik, 1. Semester"); // Keinen Zeilenumbruch am Ende
+  }
+}
