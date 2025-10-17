359249a8e234bfff1ef9025d22d9172873f4a197
Fabrice Lawson-Zankli <fabricelawson-zankli@MacBook-Air-von-Fabrice.local>
2025-10-17 08:00:35 +0200

    Folders

---
+++ Uebung1/ConsoleTest.java
@@ -0,0 +1,12 @@
+public class ConsoleTest {
+  public static void main(String[] args) {
+    String name = "Fabrice";
+    float kommazahl = 259.418f;
+
+    System.out.println(name + " " + kommazahl);
+
+    for (int i = 0; i < 10; ++i) {
+      System.out.println(i);
+    }
+  }
+}
